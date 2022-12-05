66a44ce036ab85b8229e84a04003b4deb76040b5
Kapil Saxena <saxenak737@gmail.com>
2022-12-05 15:54:00 +0530

    Vercel settings

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo "BUILD START"
+echo "Building the project..."
 python3.11.0 -m pip install -r requirements.txt
+
+echo "Make Migrations..."
+python3.11.0 manage.py makemigrations --noinput
+python3.11.0 manage.py migrate --noinput
+
+echo "Collect static..."
 python3.11.0 manage.py collectstatic --noinput --clear
 echo "BUILD END"
